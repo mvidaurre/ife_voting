414443b79351ed78bccecbc47d8facb1a983d863
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-07 19:53:43 -0500

    Green Implementation of: Candidate and IFE Card specs

--- lib/domain/entities/candidate.rb
+++ lib/domain/entities/candidate.rb
@@ -1,2 +1,7 @@
 class Candidate
+  attr_accessor :name, :alias, :dr_evil
+  
+  def initialize
+    
+  end
 end
--- lib/domain/entities/ife_card.rb
+++ lib/domain/entities/ife_card.rb
@@ -1,3 +1,22 @@
 class IfeCard
+  attr_accessor :first_name, :last_name, :dob, :state_of_birth, :sex
+  
+  def initialize
+    
+  end
 
+  def clave
+    clave = last_name[0..1] \
+          + first_name[0..1] \
+          + dob.year.to_s[2..3] \
+          + dob.month.to_s \
+          + dob.day.to_s \
+          + sex \
+          + state_of_birth \
+          + first_name[2..3] \
+          + last_name[2..3] \
+          + last_name.length.to_s \
+          + first_name.length.to_s
+    clave.upcase
+  end
 end
