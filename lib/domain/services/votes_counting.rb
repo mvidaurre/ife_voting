d6c35fa9a05dcc93924060c8e4d81ffffcfd70b8
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-07 20:01:37 -0500

    Green Implementation of: Votes Counting Service specs

--- lib/domain/services/votes_counting.rb
+++ lib/domain/services/votes_counting.rb
@@ -1,3 +1,11 @@
 class VotesCounting
-
+  def initialize
+    @urnas = []
+  end
+  def <<(urna)
+    @urnas << urna
+  end
+  def votes(political_party)
+    @urnas.inject(0){|total, v| total + v.votes_counting(political_party)}
+  end
 end
