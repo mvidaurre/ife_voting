bd9ace4146b97340920c9bb3c58d90711c1cecf4
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-07 20:08:16 -0500

    Green Implementation of: Ballot Aggregate specs

--- lib/domain/aggregates/ballot.rb
+++ lib/domain/aggregates/ballot.rb
@@ -1,3 +1,20 @@
 class Ballot
+  attr_accessor :options
+  
+  def initialize
+    @options =[]
+    @vote = Hash.new
+  end
+  
+  def <<(political_party)
+    @options << political_party
+  end
 
+  def voting(voter, political_party)
+    @vote[:voter] = political_party
+  end
+  
+  def vote(voter)
+    @vote[:voter]
+  end
 end
